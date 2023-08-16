dbaf8804c6381891b49a36dca538635cefe7dd34
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-16 15:54:04 +0600

    mongoose connection done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express()
+const port = process.env.PORT
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Mongoose connection
+
+mongoose.connect(process.env.MONGODB_DRIVER)
+.then(()=>{
+    console.log("Mongoose connected successfully")
+})
+.catch((err)=>{
+    console.log(err.message)
+})
+
+
+app.listen(port,()=>{
+    console.log(`PORT is running in ${port}`)
+})
